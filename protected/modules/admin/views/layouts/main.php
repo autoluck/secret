4ed2e5b05dbf727063dc240b5926987ab960b58d
jiangtao <847837639@qq.com>
2016-09-29 16:48:03 +0800

    update

--- protected/modules/admin/views/layouts/main.php
+++ protected/modules/admin/views/layouts/main.php
@@ -62,38 +62,13 @@
                 <ul class="nav navbar-nav pull-right">
                     <li class="dropdown dropdown-user dropdown-dark">
                         <a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="true">
-						<span class="username username-hide-on-mobile"> Nick </span>
+						<span class="username username-hide-on-mobile"> <?php echo Yii::app()->user->name; ?> </span>
                             <img alt="" class="img-circle" src="<?php echo $assetsUrl ?>/img/avatar9.jpg"/>
                         </a>
                         <ul class="dropdown-menu dropdown-menu-default">
                             <li>
-                                <a href="extra_profile.html">
-                                    <i class="icon-user"></i> My Profile </a>
-                            </li>
-                            <li>
-                                <a href="page_calendar.html">
-                                    <i class="icon-calendar"></i> My Calendar </a>
-                            </li>
-                            <li>
-                                <a href="inbox.html">
-                                    <i class="icon-envelope-open"></i> My Inbox <span class="badge badge-danger">
-								3 </span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="page_todo.html">
-                                    <i class="icon-rocket"></i> My Tasks <span class="badge badge-success">
-								7 </span>
-                                </a>
-                            </li>
-                            <li class="divider"></li>
-                            <li>
-                                <a href="extra_lock.html">
-                                    <i class="icon-lock"></i> Lock Screen </a>
-                            </li>
-                            <li>
                                 <a href="<?php echo $this->createUrl('/admin/sign/logout'); ?>">
-                                    <i class="icon-key"></i> Log Out </a>
+                                    <i class="icon-key"></i> 退出 </a>
                             </li>
                         </ul>
                     </li>
